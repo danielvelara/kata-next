5a924dadd2069131652d6ec40cb99cabe406c376
Daniel Velázquez <13500134+danielvelara@users.noreply.github.com>
2023-05-26 13:40:44 -0600

    May26

---
+++ src/pages/auth/premium.tsx
@@ -0,0 +1,18 @@
+import { signOut, useSession } from "next-auth/react";
+
+type Props = {};
+export default function Premium({}: Props) {
+  const { data: session, status } = useSession();
+  if (status == "loading") {
+    return <p>Loading</p>;
+  }
+  if (session == null) {
+    return <p>Denied</p>;
+  }
+  return (
+    <div>
+      <button onClick={() => signOut()}>Sign out</button>
+      <p>Secret content</p>
+    </div>
+  );
+}
---
+++ src/pages/auth/profile.tsx
@@ -0,0 +1,23 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+type Props = {};
+export default function Profile({}: Props) {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        <p>
+          Signed in as {session.user?.name} = {session.user?.email}
+        </p>
+        <br />
+        <button onClick={() => signOut}>Sign out</button>
+      </>
+    );
+  }
+  return (
+    <div className="grid grid-cols-3 justify-evenly bg-slate-500">
+      <div>First</div>
+      <div>Second</div>
+      <button onClick={() => signIn()}>Sign in</button>
+    </div>
+  );
+}
